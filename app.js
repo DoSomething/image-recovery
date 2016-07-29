854835bd3540008ea285f6ba36fbbfb93b94bd04
Aaron Schachter <aaronschachter@gmail.com>
2016-07-29 12:08:58 -0700

    Starts on getCampaignIdForOptInPath

--- app.js
+++ app.js
@@ -35,12 +35,33 @@ function parseMessage(index, messages, cb) {
     cb();
     return;
   }
-
-  console.log(JSON.stringify(message));
+  // console.log(JSON.stringify(message))
+  addReportback(message);   
   // Temporary to prevent max stack exceed, once the actual async functions are in this timeout can go
   setTimeout(function() {
     parseMessage(index++, messages, cb);
   }, 100);
+}
+
+function addReportback(message) {
+  var oip = message.keyword[0].opt_in_path_id;
+  console.log('time' + message.received_at);
+  console.log('phone ' + message.phone_number);
+  console.log('oip ' + oip);
+  console.log('campaign ' + getCampaignIdForOptInPath(oip))
+  console.log('url ' + message.mms[0].image_url);
+  return;
+}
+
+function getCampaignIdForOptInPath(optInPath) {
+  var campaignId;
+  switch (parseInt(optInPath)) {
+    // Notes For Shawn
+    case 209633:
+      campaignId = 2805;
+      break;
+  }
+  return campaignId;
 }
 
 function getMessages(page) {
